2708815954a83e66f1fc365f057b01ba9ff6ad76
safeer239 <mohammedsafeershefi@gamil.com>
2024-05-13 16:53:56 +0530

    create sighnup api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from "./routes/userRoute.js"
+import authRoutes from "./routes/authRoute.js"
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,7 +13,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000,() => {
     console.log("Server listening on the 3000")
 })
+
+app.use("/server/user/",userRoutes)
+app.use("/server/auth/",authRoutes)
